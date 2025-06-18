3643a8b29a6a777e9ac4d1397c8c32870b7d8620
Mehedi Hasan <mehedi.hsn.dev.@gmail.com>
2025-06-18 14:33:31 +0600

    Create and Save a Note, Get All and Single Notes

--- practice-1/src/routes/note.routes.ts
+++ practice-1/src/routes/note.routes.ts
@@ -3,6 +3,7 @@ import Note from '../models/note';
 
 const router = express.Router();
 
+// ✅ Create note
 router.post('/create-note', async (req, res) => {
   try {
     const newNote = new Note(req.body);
@@ -10,6 +11,29 @@ router.post('/create-note', async (req, res) => {
     res.status(201).json({ status: true, message: 'Note saved successfully!' });
   } catch (error) {
     res.status(400).json({ status: false, message: 'Error saving note!' });
+  }
+});
+
+// ✅ Get all notes
+router.get('/notes', async (req, res) => {
+  try {
+    const notes = await Note.find();
+    res
+      .status(200)
+      .json({ status: true, message: 'Note saved successfully!', data: notes });
+  } catch (error) {
+    res.status(400).json({ status: false, message: 'Failed to get all notes' });
+  }
+});
+
+// ✅ Get single note by id
+router.get('/notes/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const note = await Note.findById(id);
+    res.status(200).json({ status: true, data: note });
+  } catch (error) {
+    res.status(400).json({ status: false, message: 'Failed to get note' });
   }
 });
 
